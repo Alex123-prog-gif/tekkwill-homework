14fb3b6e0f97271f67bb124c2fc4f70c20f2b0ba
Alexandru Oglinda <ogycat2021@icloud.com>
2021-03-19 14:08:10 +0200

    another commit

---
+++ homework/movies/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+
+                System.out.println("My favorite movie is joker");
+                System.out.println("IT comes from USA");
+                System.out.println("It was made in 2020");
+                System.out.println("");
+                System.out.println("x                      x");
+                System.out.println("x                      x") ;
+                System.out.println("x     xxxxxxxxxx       x ");
+                System.out.println("xxxxx x        x   xxxxx");
+                System.out.println("x   x x        x   x   x");
+                System.out.println("x   x x        x   x   x");
+
+            }
+        }
+    
+
