31278e7135635064277f6a0df707e0f9fe993767
Alexandru Oglinda <ogycat2021@icloud.com>
2021-03-30 17:09:11 +0300

    woow

--- homework/Personalproject/src/PROJECT.java
+++ homework/Personalproject/src/PROJECT.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class PROJECT {
+import java.util.Scanner;
+
+public class PROJECT {
+    public static void main(String[] args) {
+        System.out.println("Hello my friend");
+        System.out.println("I'm named the Chatty-bot");
+        System.out.println("I was created during the Java Fundamentals course of March-June 2021.Born during the COVID pandemic!Can you believe that");
+        System.out.println("Whats your name?");
+        Scanner scanner = new Scanner(System.in);
+        String Alex = scanner.nextLine();
+        System.out.println(Alex);
+        System.out.println("What's your age?");
+        System.out.println("Put 7 to your age and i will guess it");
+        int userAge = scanner.nextInt();
+        System.out.println("Your age is " + (userAge - 7));
+
+
+
+        
+        
+    }
 }
--- homework/Statements/src/com/company/Ex5.java
+++ homework/Statements/src/com/company/Ex5.java
@@ -1,14 +1,2 @@
-package com.company;
-
-import java.util.Scanner;
-
-public class Ex5 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int secondnumber = 30;
-        int firstnumber = 20;
-        System.out.println(firstnumber + secondnumber);
-        System.out.println(firstnumber / secondnumber);
-    }
+package com.company;public class Ex5 {
 }
